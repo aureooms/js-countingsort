fd7be47fab2140e13943b8c49a5a204aac78a28e
aureooms <aurelien.ooms@gmail.com>
2015-06-22 12:01:42 +0200

    v0.0.1

---
+++ js/dist/countingsort.js
@@ -0,0 +1,22 @@
+( function ( ) {
+
+'use strict' ;
+
+var definition = function ( exports , undefined ) {
+
+
+/* js/src/dummy.js */
+
+return exports ;
+} ;
+if ( typeof exports === "object" ) {
+	definition( exports ) ;
+}
+else if ( typeof define === "function" && define.amd ) {
+	define( "aureooms-js-countingsort" , [ ] , function ( ) { return definition( { } ) ; } ) ;
+}
+else if ( typeof window === "object" && typeof window.document === "object" ) {
+	definition( window["countingsort"] = { } ) ;
+}
+else console.error( "unable to detect type of module to define for aureooms-js-countingsort") ;
+} )( ) ;
