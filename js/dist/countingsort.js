9f350e4408142139a081db8348ec646b8b73dde1
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-02-16 11:35:46 +0100

    aureooms- > @aureooms/ : update js code

--- js/dist/countingsort.js
+++ js/dist/countingsort.js
@@ -13,10 +13,10 @@ if ( typeof exports === "object" ) {
 	definition( exports ) ;
 }
 else if ( typeof define === "function" && define.amd ) {
-	define( "aureooms-js-countingsort" , [ ] , function ( ) { return definition( { } ) ; } ) ;
+	define( "@aureooms/js-countingsort" , [ ] , function ( ) { return definition( { } ) ; } ) ;
 }
 else if ( typeof window === "object" && typeof window.document === "object" ) {
 	definition( window["countingsort"] = { } ) ;
 }
-else console.error( "unable to detect type of module to define for aureooms-js-countingsort") ;
+else console.error( "unable to detect type of module to define for @aureooms/js-countingsort") ;
 } )( ) ;
